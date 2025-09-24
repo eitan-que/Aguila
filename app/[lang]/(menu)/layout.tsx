aff6259126ca19b0840df7d9b9aacc14a6a6e208
eitan-que <eitanfeldman021@gmail.com>
2025-09-24 20:00:19 -0300

    Update layout.tsx

--- app/[lang]/(menu)/layout.tsx
+++ app/[lang]/(menu)/layout.tsx
@@ -1,7 +1,13 @@
 import { cookies, headers } from "next/headers";
 import LoginDialog from "@/components/auth/login-dialog";
 import { auth } from "@/lib/auth";
-import { getDictionary, Lang } from "@/actions/dictionaries";
+import { getDictionary, Lang, locales } from "@/actions/dictionaries";
+
+export const revalidate = 3600 // 1 hour
+
+export async function generateStaticParams() {
+  return locales.map((lang) => ({ lang }))
+}
 
 export default async function MenuLayout({
   children,
